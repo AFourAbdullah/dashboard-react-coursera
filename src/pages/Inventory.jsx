0a7a3e37ac189705d5df601825a50b4a14750bd4
Abdullah Aziz <abdullahazees4@gmail.com>
2023-09-05 18:04:40 +0500

    code added

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,8 +1,76 @@
-import { Typography } from "antd";
-import React from "react";
+import { Avatar, Rate, Space, Table, Typography } from "antd";
+import React, { useEffect, useState } from "react";
+import { getInventory } from "../api";
 
 const Inventory = () => {
-  return <Typography.Title level={3}>Inventory</Typography.Title>;
+  const [dataSource, setdataSource] = useState(0);
+  const [loading, setloading] = useState(false);
+  useEffect(() => {
+    setloading(true);
+    getInventory().then((res) => setdataSource(res.products));
+    setloading(false);
+  }, []);
+  return (
+    <Space size={20} direction="vertical">
+      <Typography.Title
+        level={3}
+        style={{ textAlign: "center", fontWeight: "bold", fontSize: "30px" }}
+      >
+        Inventory
+      </Typography.Title>
+      <Table
+        style={{
+          width: "80vw",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+        dataSource={dataSource}
+        pagination={{
+          pageSize: 6,
+          style: {
+            display: "flex",
+            justifyContent: "center",
+          },
+        }}
+        loading={loading}
+        columns={[
+          {
+            title: "Thumbnail",
+            dataIndex: "thumbnail",
+            render: (link) => {
+              return <Avatar src={link} />;
+            },
+          },
+          {
+            title: "Title",
+            dataIndex: "title",
+          },
+          {
+            title: "Price",
+            dataIndex: "price",
+            render: (value) => <span>${value}</span>,
+          },
+          {
+            title: "Rating",
+            dataIndex: "rating",
+            render: (rating) => {
+              return <Rate value={rating} allowHalf disabled />;
+            },
+          },
+          {
+            title: "Stock",
+            dataIndex: "stock",
+          },
+
+          {
+            title: "Brand",
+            dataIndex: "brand",
+          },
+        ]}
+      ></Table>
+    </Space>
+  );
 };
 
 export default Inventory;
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,8 +1,62 @@
-import { Typography } from "antd";
-import React from "react";
+import { Avatar, Rate, Space, Table, Typography } from "antd";
+import React, { useEffect, useState } from "react";
+import { getInventory, getOrders } from "../api";
 
 const Orders = () => {
-  return <Typography.Title level={3}>Orders</Typography.Title>;
+  const [dataSource, setdataSource] = useState(0);
+  const [loading, setloading] = useState(false);
+  useEffect(() => {
+    setloading(true);
+    getOrders().then((res) => setdataSource(res.products));
+    setloading(false);
+  }, []);
+  return (
+    <Space size={20} direction="vertical">
+      <Typography.Title
+        level={3}
+        style={{ textAlign: "center", fontWeight: "bold", fontSize: "30px" }}
+      >
+        Orders
+      </Typography.Title>
+      <Table
+        style={{
+          width: "80vw",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+        dataSource={dataSource}
+        pagination={{
+          pageSize: 6,
+          style: {
+            display: "flex",
+            justifyContent: "center",
+          },
+        }}
+        loading={loading}
+        columns={[
+          {
+            title: "Title",
+            dataIndex: "title",
+          },
+          {
+            title: "Quantity",
+            dataIndex: "quantity",
+          },
+          {
+            title: "Total",
+            dataIndex: "total",
+            render: (value) => <span>${value}</span>,
+          },
+          {
+            title: "Discounted Price",
+            dataIndex: "discountedPrice",
+            render: (value) => <span>${value}</span>,
+          },
+        ]}
+      ></Table>
+    </Space>
+  );
 };
 
 export default Orders;
