14ccb27e00b0c63bfe6a64fab6fbd6830024a3c3
Abdullah Aziz <abdullahazees4@gmail.com>
2023-09-05 18:22:32 +0500

    code added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,16 +5,22 @@ import {
   UserOutlined,
 } from "@ant-design/icons";
 import { Menu, Switch } from "antd";
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
   const [theme, setTheme] = useState("dark");
   const [current, setCurrent] = useState("1");
+  const [selectedKeys, setSelectedKeys] = useState("/");
   const changeTheme = (value) => {
     setTheme(value ? "dark" : "light");
   };
   const navigate = useNavigate();
+  const location = useLocation();
+  useEffect(() => {
+    const pathName = location.pathname;
+    setSelectedKeys(pathName);
+  }, [location.pathname]);
   return (
     <div className="SideMenu">
       <Switch
@@ -27,6 +33,7 @@ const Sidebar = () => {
         }}
       />
       <Menu
+        selectedKeys={[selectedKeys]}
         style={{
           width: 256,
           height: "65vh",
@@ -37,12 +44,13 @@ const Sidebar = () => {
           borderRadius: "20px",
           marginTop: "20px",
           marginLeft: "5px",
+          fontSize: "20px",
         }}
         theme={theme}
         onClick={(item) => navigate(item.key)}
         items={[
           {
-            label: "Dashbaord",
+            label: "Dashboard",
             icon: <AppstoreOutlined />,
             key: "/",
           },
