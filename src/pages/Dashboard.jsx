a467c067367c143c100284599266d0125e34a2d0
Abdullah Aziz <abdullahazees4@gmail.com>
2023-09-05 17:11:23 +0500

    code added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,148 @@
-import { Typography } from "antd";
+import {
+  DollarCircleOutlined,
+  ShoppingCartOutlined,
+  ShoppingOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Card, Space, Statistic, Table, Typography } from "antd";
+import React, { useEffect, useState } from "react";
+import { getOrders } from "../api";
 import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const Dashboard = () => {
-  return <Typography.Title level={3}>Dashboard</Typography.Title>;
+  return (
+    <Space size={20} direction="vertical">
+      <Typography.Title level={3}>Dashboard</Typography.Title>
+      <Space direction="horizontal">
+        <DashbaordCard
+          icon={
+            <ShoppingCartOutlined
+              style={{
+                color: "green",
+                backgroundColor: "rgba(0,255,0,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+              }}
+            />
+          }
+          title="Orders"
+          value="1234"
+        />
+        <DashbaordCard
+          icon={
+            <ShoppingOutlined
+              style={{
+                color: "blue",
+                backgroundColor: "rgba(0,0,255,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+              }}
+            />
+          }
+          title="Inventory"
+          value="12345"
+        />
+        <DashbaordCard
+          icon={
+            <UserOutlined
+              style={{
+                color: "purple",
+                backgroundColor: "rgba(0,255,255,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+              }}
+            />
+          }
+          title="Customers"
+          value="12345"
+        />
+        <DashbaordCard
+          icon={
+            <DollarCircleOutlined
+              style={{
+                color: "red",
+                backgroundColor: "rgba(255,0,0,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+              }}
+            />
+          }
+          title="Revenue"
+          value="12345"
+        />
+      </Space>
+      <Space>
+        <RecentOrders />
+      </Space>
+    </Space>
+  );
 };
+function DashbaordCard({ title, value, icon }) {
+  return (
+    <Card>
+      <Space direction="horizontal">
+        {icon}
+        <Statistic title={title} value={value} />
+      </Space>
+    </Card>
+  );
+}
 
+function RecentOrders() {
+  const [orders, setOrders] = useState(0);
+  const [loading, setloading] = useState(false);
+  useEffect(() => {
+    setloading(true);
+    getOrders().then((res) => {
+      setOrders(res.products.splice(0, 3));
+      setloading(false);
+    });
+  }, []);
+  return (
+    <>
+      <Typography.Text>Recent Orders</Typography.Text>
+      <Table
+        columns={[
+          {
+            title: "Title",
+            dataIndex: "title",
+          },
+          {
+            title: "Quantity",
+            dataIndex: "quantity",
+          },
+          {
+            title: "Price",
+            dataIndex: "discountedPrice",
+          },
+        ]}
+        loading={loading}
+        dataSource={orders}
+        pagination={false}
+      ></Table>
+    </>
+  );
+}
 export default Dashboard;
